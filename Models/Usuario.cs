3b0017d9c19eb399042a50cb4357c22a181e69f0
Ari Beri <berinsteinariel@gmail.com>
2024-11-16 16:44:39 -0300

    Add project files.

---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace VirvisShopFinal.Models
+{
+    public class Usuario
+    {
+
+    }
+}

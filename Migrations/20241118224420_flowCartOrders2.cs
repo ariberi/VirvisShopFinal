964f7d3617adb652c470e6550df20132258802fe
01agustin <agustin_9_d@hotmail.com>
2024-11-19 18:09:46 -0300

    Reapply "."
    This reverts commit efdfc302face60254f7ed421991f525aaf161982.

---
+++ Migrations/20241118224420_flowCartOrders2.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirvisShopFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class flowCartOrders2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_id",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_id",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_id",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_id",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Carts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_id",
+                table: "Orders",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_id",
+                table: "Carts",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_id",
+                table: "Carts",
+                column: "id",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_id",
+                table: "Orders",
+                column: "id",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Migrations/20241118230642_inicial2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirvisShopFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Userid",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_Userid",
+                table: "Carts",
+                column: "Userid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_Userid",
+                table: "Carts",
+                column: "Userid",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_Userid",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_Userid",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "Userid",
+                table: "Carts");
+        }
+    }
+}

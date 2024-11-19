efdfc302face60254f7ed421991f525aaf161982
01agustin <agustin_9_d@hotmail.com>
2024-11-19 18:08:22 -0300

    Revert "."
    This reverts commit 72e836e4e22fc0b15722c899380ae108554e250c.

---
+++ Migrations/20241118210211_VirvisShopFinal.Context.VirvisDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirvisShopFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class VirvisShopFinalContextVirvisDatabaseContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

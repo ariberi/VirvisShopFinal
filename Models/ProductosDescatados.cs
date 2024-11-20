6f306974433d8d39e826b4377073d7fad01666be
01agustin <agustin_9_d@hotmail.com>
2024-11-20 17:01:26 -0300

    add model and view of productos destacados

---
+++ Models/ProductosDescatados.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VirvisShopFinal.Models
+{
+    public class ProductosDescatados
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int codDestacado { get; set; }
+
+        public required Product Product { get; set; }
+
+    }
+}
